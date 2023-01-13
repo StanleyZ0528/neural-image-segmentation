3b48cf8c9a9ebf495efa45e8b94e8d67a20d305f
Chao Wang <62739093+ChrischaoWang@users.noreply.github.com>
2023-01-14 01:33:32 +0800

    Add data resample func

--- neural-image-segmentation/unet/unet_utils.py
+++ neural-image-segmentation/unet/unet_utils.py
@@ -20,8 +20,28 @@ def gamma_correction(input_image):
     img_gamma = np.power(input_image, gamma).clip(0, 255).astype(np.uint8)
     return img_gamma
 
+def image_resample(input_image_dir, size = (5,4), sample_size = 512):
+    # crop the given image to targe samples
+    img = cv2.imread(input_image_dir)
+    image_samples = []
+    temp_np = np.array(img)
+    # row
+    for row in range(size[0]):
+        # col
+        for col in range(size[1]):
+            r_start = row*sample_size
+            r_stop = r_start+sample_size
+            c_start = col*sample_size
+            c_stop = c_start+sample_size
+            if c_stop > len(img):
+                c_stop = len(img)
+                c_start = c_stop-sample_size
+            
+            cur_sample_np = temp_np[c_start:c_stop,r_start:r_stop]
+
+            # add results
+            image_samples.append(cur_sample_np)
+    return image_samples
 
 def unet_predict(input_image):
     return unet_model(input_image[None,:]).argmax(dim=1)[0].detach().numpy()
-
-
