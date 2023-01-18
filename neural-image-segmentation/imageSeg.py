a419e5ba489936b6458c501b98e9b7094f26595a
Stanley <stz2805@gmail.com>
2023-01-17 23:45:52 -0500

    Adding Image Post Processing to UI

--- neural-image-segmentation/imageSeg.py
+++ neural-image-segmentation/imageSeg.py
@@ -6,6 +6,8 @@ from PyQt6 import QtWidgets, QtCore, QtGui
 from PIL import Image, ImageQt
 from UI.ut import Ui_MainWindow
 from unet.unet_utils import gamma_correction
+from postImgProc.utils import *
+from postImgProc.alg import *
 
 
 class ImgSeg(QtWidgets.QMainWindow):
@@ -14,14 +16,26 @@ class ImgSeg(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow(self)
         self.ui.toolButton.clicked.connect(self.tool_button_callback)
         self.ui.pushButton.clicked.connect(self.process_button_callback)
+        self.ui.analyzeButton.clicked.connect(self.analyze_button_callback)
         self.ui.clearButton.clicked.connect(self.clear_button_callback)
         self.file_name = ""
+        self.img = []
         self.show()
+
+    def display_img(self, image):
+        img = Image.fromarray(image, mode='RGB')
+        qt_img = ImageQt.ImageQt(img)
+        pix = QtGui.QPixmap.fromImage(qt_img)
+        item = QtWidgets.QGraphicsPixmapItem(pix)
+        scene = QtWidgets.QGraphicsScene(self)
+        scene.addItem(item)
+        self.ui.graphicsView.setScene(scene)
 
     def tool_button_callback(self):
         self.file_name = QtWidgets.QFileDialog.getOpenFileName(self)[0]
         self.ui.textBrowser.setText(self.file_name)
         if self.file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.tif', '.bmp')):
+            self.img = readImg(self.file_name)
             pix = QtGui.QPixmap(self.file_name)
             item = QtWidgets.QGraphicsPixmapItem(pix)
             scene = QtWidgets.QGraphicsScene(self)
@@ -41,13 +55,7 @@ class ImgSeg(QtWidgets.QMainWindow):
             # more process ...
 
             # display the result
-            img = Image.fromarray(result, mode='RGB')
-            qt_img = ImageQt.ImageQt(img)
-            pix = QtGui.QPixmap.fromImage(qt_img)
-            item = QtWidgets.QGraphicsPixmapItem(pix)
-            scene = QtWidgets.QGraphicsScene(self)
-            scene.addItem(item)
-            self.ui.graphicsView.setScene(scene)
+            self.display_image(result)
         else:
             msg = QtWidgets.QMessageBox(self)
             msg.setWindowTitle("Failed")
@@ -58,6 +66,31 @@ class ImgSeg(QtWidgets.QMainWindow):
         self.ui.textBrowser.setText("")
         scene = QtWidgets.QGraphicsScene(self)
         self.ui.graphicsView.setScene(scene)
+
+    def analyze_button_callback(self):
+        labeled_axon, labeled_cell, nr_cell = separate_axon_and_cell(self.img)
+        axons_to_cells = get_touching_dict(labeled_axon, labeled_cell)
+        fil = filter_axon(self.img)
+        info_list = analyze_axons(fil, labeled_axon, axons_to_cells)
+        segmented_axons = getSegmentedAxons(fil, info_list, nr_cell)
+        img_annotated = self.img.copy()
+        print(img_annotated)
+        # for item in info_list:
+        #     touch_points = item["touch_points"]
+        #     intersect_points = item["intersect_points"]
+        #     end_points = item["end_points"]
+        #     for coord in touch_points:
+        #         img_annotated[coord[0]][coord[1]] = [255, 0, 0]
+        #     for coord in intersect_points:
+        #         if not type(coord) is tuple:
+        #             continue
+        #         img_annotated[coord[0]][coord[1]] = [0, 0, 255]
+        #     for coord in end_points:
+        #         if not type(coord) is tuple:
+        #             continue
+        #         img_annotated[coord[0]][coord[1]] = [0, 255, 0]
+        # display the result
+        self.display_img(img_annotated)
 
 
 if __name__ == '__main__':
